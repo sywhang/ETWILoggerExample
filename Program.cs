4aec3e347942c78001ce3d9f566ad6f715d95315
Sung Yoon Whang <suwhang@microsoft.com>
2021-02-24 17:50:24 -0800

    Add realtime example

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Session;
 
@@ -7,15 +8,8 @@ namespace ETWILoggerExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static void TraceUsingFile(int traceDuration)
         {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Usage: dotnet run [traceDurationInSec]");
-                return;
-            }
-
-            int traceDuration = Int32.Parse(args[0]);
             using (var session = new TraceEventSession("ILoggerSession", "MyEventData.etl"))
             {
                 session.EnableProvider("Microsoft-Extensions-Logging", TraceEventLevel.Verbose, ulong.MaxValue, new TraceEventProviderOptions(new String[] { "FilterSpecs", "App*:Information;*" }));
@@ -28,6 +22,44 @@ namespace ETWILoggerExample
                 };
                 source.Process();
             }
+        }
+        static void TraceRealTime(int traceDuration)
+        {
+            TraceEventSession session = new TraceEventSession("ILoggerSession");
+            Task ParserTask = Task.Run(() => {
+                session.Source.Dynamic.All += delegate(TraceEvent data) {
+                    Console.WriteLine("ILogger Event: {0}", data);
+                };
+                session.EnableProvider("Microsoft-Extensions-Logging", TraceEventLevel.Verbose, ulong.MaxValue, new TraceEventProviderOptions(new String[] { "FilterSpecs", "App*:Information;*" }));
+                session.Source.Process();
+            });
+
+            Thread.Sleep(traceDuration * 1000);
+            session.Stop();
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: dotnet run [file|rt] [traceDurationInSec]");
+                return;
+            }
+            string mode = args[0];
+            int traceDuration = Int32.Parse(args[1]);
+
+            if (mode == "file")
+            {
+                TraceUsingFile(traceDuration);
+            }
+            else if (mode == "rt")
+            {
+                TraceRealTime(traceDuration);
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode. Use either file or rt mode");
+            }
+
         }
     }
 }
