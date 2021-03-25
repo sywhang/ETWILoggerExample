6de3bb8005498772ebefa782b26fb74b47c3219a
Sung Yoon Whang <suwhang@microsoft.com>
2021-03-25 14:34:22 -0700

    updated the parser to be more specific about payload name

--- Program.cs
+++ Program.cs
@@ -8,34 +8,54 @@ namespace ETWILoggerExample
 {
     class Program
     {
+        static void ParserDelegate(TraceEvent data) {
+            switch (data.EventName)
+            {
+                case "FormattedMessage":
+                    Console.WriteLine($"[FormattedMessage] {data.PayloadStringByName("LoggerName")} : {data.PayloadStringByName("FormattedMessage")}");
+                    break;
+                case "Message":
+                    Console.WriteLine($"[Message] {data.PayloadStringByName("LoggerName")} : {data.PayloadStringByName("EventName")}");
+                    break;
+                // There are other types of events that are logged by ILogger but won't print them since
+                // they're irrelevant (i.e. ActivityStart, ActivityEnd, etc.) or are dups 
+                // (ex. "MessageJson" which is just a Json formatted string of "Message")
+                // You can add more stuff here as needed.
+                default:
+                    break;
+            }
+        }
+
         static void TraceUsingFile(int traceDuration)
         {
-            using (var session = new TraceEventSession("ILoggerSession", "MyEventData.etl"))
+            string tempFile = "MyEventData.etl";
+            using (var session = new TraceEventSession("ILoggerSession", tempFile))
             {
                 session.EnableProvider("Microsoft-Extensions-Logging", TraceEventLevel.Verbose, ulong.MaxValue, new TraceEventProviderOptions(new String[] { "FilterSpecs", "App*:Information;*" }));
 
                 Thread.Sleep(traceDuration * 1000);
             }
-            using (var source = new ETWTraceEventSource("MyEventData.etl")) {
-                source.Dynamic.All += delegate(TraceEvent data) {
-                    Console.WriteLine("ILogger Event: {0}", data);
-                };
-                source.Process();
-            }
+            ParseFile(tempFile);
         }
         static void TraceRealTime(int traceDuration)
         {
             TraceEventSession session = new TraceEventSession("ILoggerSession");
             Task ParserTask = Task.Run(() => {
-                session.Source.Dynamic.All += delegate(TraceEvent data) {
-                    Console.WriteLine("ILogger Event: {0}", data);
-                };
+                session.Source.Dynamic.All += ParserDelegate;
                 session.EnableProvider("Microsoft-Extensions-Logging", TraceEventLevel.Verbose, ulong.MaxValue, new TraceEventProviderOptions(new String[] { "FilterSpecs", "App*:Information;*" }));
                 session.Source.Process();
             });
 
             Thread.Sleep(traceDuration * 1000);
             session.Stop();
+        }
+
+        static void ParseFile(string filePath)
+        {
+            using (var source = new ETWTraceEventSource(filePath)) {
+                source.Dynamic.All += ParserDelegate;
+                source.Process();
+            }
         }
         static void Main(string[] args)
         {
@@ -46,7 +66,6 @@ namespace ETWILoggerExample
             }
             string mode = args[0];
             int traceDuration = Int32.Parse(args[1]);
-
             if (mode == "file")
             {
                 TraceUsingFile(traceDuration);
@@ -59,7 +78,6 @@ namespace ETWILoggerExample
             {
                 Console.WriteLine("Invalid mode. Use either file or rt mode");
             }
-
         }
     }
 }
